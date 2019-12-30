4a5492643e030ca2775680c466b5954f88c73303
sagi levy <sagilevy@sagis-MacBook-Pro.local>
2019-12-30 18:36:18 +0200

    tag_ec2 and tag_trail

--- function_handler.py
+++ function_handler.py
@@ -14,14 +14,21 @@ def handler(event, context):
     # tag s3 buckets
     if (eventJson["eventSource"] == 's3.amazonaws.com' and
         eventJson["eventName"] == 'CreateBucket'):
+        print("s3")
         # Run tag_s3 function
         result = tag_s3(eventJson["requestParameters"]["bucketName"], tags)
     # tag ec2 instances
     elif eventJson["eventSource"] == "ec2.amazonaws.com":
+        print("ec2")
         # Run tag_ec2 function
         result = tag_ec2(eventJson["eventName"], eventJson["responseElements"],tags)
-        if eventJson["eventName"] == "CreateVpc":
-            result = "Vpc"
+    # tag CreateTrail trails
+    elif (eventJson["eventSource"] == "cloudtrail.amazonaws.com" and
+        eventJson["eventName"] == 'CreateTrail'):
+        print("cloudtrail")
+        # Run tag_trail function
+        result = tag_trail(eventJson["responseElements"],tags)
+    
     # print output
     print(result)
 
@@ -37,10 +44,11 @@ def tag_ec2(eventName, responseElements, tags):
     ids = []
 
     if eventName == "RunInstances":
+        #loop through instances"
         for item in responseElements["instancesSet"]["items"]:
             ids.append(item['instanceId'])
         base = ec2.instances.filter(InstanceIds=ids)
-        #loop through the instances",
+        #loop through connected resources"
         for instance in base:
             for vol in instance.volumes.all():
                 ids.append(vol.id)
@@ -52,6 +60,14 @@ def tag_ec2(eventName, responseElements, tags):
         ids.append(responseElements['imageId'])
     elif eventName == 'CreateSnapshot':
         ids.append(responseElements['snapshotId'])
+    elif eventName == 'CreateInternetGateway':
+        ids.append(responseElements['internetGateway']['internetGatewayId'])
+    elif eventName == 'CreateSecurityGroup':
+        ids.append(responseElements['groupId'])
+    elif eventName == 'CreateNetworkAcl':
+        ids.append(responseElements['networkaclid'])
+    elif eventName == 'CreateVpc':
+        ids.append(responseElements['vpcid'])
     if ids:
         return ec2.create_tags(
                     Resources=ids,
@@ -59,7 +75,7 @@ def tag_ec2(eventName, responseElements, tags):
                 )
     return "No IDs"
 
-# Tag All S3 Buckets
+# Tag S3 Bucket
 def tag_s3(bucketName, tags):
     s3 = boto3.client('s3')
 
@@ -68,4 +84,12 @@ def tag_s3(bucketName, tags):
         Tagging={
             'TagSet': tags
             }
+    )
+# Tag CreateTrail trail
+def tag_trail(responseElements, tags):
+    cloudtrail = boto3.client('cloudtrail')
+
+    return cloudtrail.add_tags(
+        ResourceId = responseElements['trailid'],
+        TagsList = tags
     )
